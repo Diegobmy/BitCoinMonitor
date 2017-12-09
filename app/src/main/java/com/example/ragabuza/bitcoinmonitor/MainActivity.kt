787bd1f45bcd052f0e88aa6e9340602c3cd7c1b3
Diego <diegobmoyses@gmail.com>
2017-12-09 02:58:19 -0200

    Formulário add notification

--- app/src/main/java/com/example/ragabuza/bitcoinmonitor/MainActivity.kt
+++ app/src/main/java/com/example/ragabuza/bitcoinmonitor/MainActivity.kt
@@ -2,11 +2,58 @@ package com.example.ragabuza.bitcoinmonitor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initSpinners()
+    }
+
+    fun initSpinners(){
+        val condition = ArrayList<String>()
+        condition.add(getString(R.string.Condition1))
+        condition.add(getString(R.string.Condition2))
+        initSpinner(spCondition, condition)
+
+        val providers = ArrayList<String>()
+        providers.add("FoxBit")
+        providers.add("Mercado Bitcoin")
+        providers.add("Negocie Coins")
+        providers.add("BitcoinToYou")
+        providers.add("BitcoinTrade")
+        providers.add("flowBTC")
+        providers.add("LocalBitcoins")
+        providers.add("Arena Bitcoin")
+        initSpinner(spProvider, providers)
+
+        val notifyTimer = ArrayList<String>()
+        notifyTimer.add("15 "+getString(R.string.Minute)+"s")
+        notifyTimer.add("30 "+getString(R.string.Minute)+"s")
+        notifyTimer.add("1 "+getString(R.string.Hour))
+        notifyTimer.add("6 "+getString(R.string.Hour)+"s")
+        notifyTimer.add("12 "+getString(R.string.Hour)+"s")
+        notifyTimer.add("24 "+getString(R.string.Hour)+"s")
+        initSpinner(spNotifyTimer, notifyTimer)
+
+        val notifyType = ArrayList<String>()
+        notifyType.add(getString(R.string.Simple))
+        notifyType.add(getString(R.string.Sound))
+        notifyType.add(getString(R.string.Loud))
+        notifyType.add(getString(R.string.Ringer))
+        initSpinner(spNotifyType, notifyType)
+    }
+
+    fun initSpinner(spinner: Spinner, list: ArrayList<String>){
+        val adapter = ArrayAdapter(
+                this, android.R.layout.simple_spinner_item, list)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
